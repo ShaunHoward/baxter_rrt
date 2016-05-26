6b206872934c54faad0339fe1006d6bec72c1947
Shaun Howard <smh150@case.edu>
2016-05-25 20:35:58 -0400

    added pcl install notes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # potential_fields
 Merry the robot with potential fields for joint control for EECS 499 Research Project.
+
+# install PCL
+* sudo add-apt-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl
+* sudo apt-get update
+* sudo apt-get install libpcl-all
--- shake_hands.py
+++ shake_hands.py
@@ -24,6 +24,15 @@ ERROR = "ERROR"
 G_HOME = Pose(1, 2, 3, 4, 5, 6)
 
 
+def start_pcl():
+    import pcl
+    p = pcl.PointCloud()
+    p.from_file("table_scene_lms400.pcd")
+    fil = p.make_statistical_outlier_filter()
+    fil.set_mean_k (50)
+    fil.set_std_dev_mul_thresh (1.0)
+    fil.filter().to_file("inliers.pcd")
+
 class Merry():
 
     curr_goal = None
