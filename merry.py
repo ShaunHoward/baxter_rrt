42761b40b3c20ec61a393f45c3757c35dfda3502
Shaun Howard <smh150@case.edu>
2016-06-11 22:30:12 -0400

    progress on merry and planner, does not build yet - math not compiling

--- merry.py
+++ merry.py
@@ -5,10 +5,13 @@ import math
 import numpy as np
 import rospy
 import std_msgs.msg
+import struct
 import tf
+import planner
 
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs import point_cloud2 as pc2
+from sensor_msgs import JointState
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import PointCloud
 from sensor_msgs.msg import PointCloud2
@@ -26,6 +29,11 @@ from geometry_msgs.msg import (
 from baxter_core_msgs.srv import (
     SolvePositionIK,
     SolvePositionIKRequest,
+)
+
+from std_msgs.msg import (
+    Header,
+    Empty,
 )
 
 # amount of time to wait for IK solver service
@@ -85,38 +93,65 @@ def transform_pcl2(tf_, target_frame, source_frame, point_cloud, duration=3):
     return [Point(*(tuple(np.dot(mat44, np.array([p[0], p[1], p[2], 1.0])))[:3])) for p in point_cloud]
 
 
+def ik_request(self, pose):
+    hdr = Header(stamp=rospy.Time.now(), frame_id='base')
+    ikreq = SolvePositionIKRequest()
+    ikreq.pose_stamp.append(PoseStamped(header=hdr, pose=pose))
+    try:
+        resp = self._iksvc(ikreq)
+    except (rospy.ServiceException, rospy.ROSException), e:
+        rospy.logerr("Service call failed: %s" % (e,))
+        return False
+    # Check if result valid, and type of seed ultimately used to get solution
+    # convert rospy's string representation of uint8[]'s to int's
+    resp_seeds = struct.unpack('<%dB' % len(resp.result_type), resp.result_type)
+    if resp_seeds[0] != resp.RESULT_INVALID:
+        seed_str = {
+            ikreq.SEED_USER: 'User Provided Seed',
+            ikreq.SEED_CURRENT: 'Current Joint Angles',
+            ikreq.SEED_NS_MAP: 'Nullspace Setpoints',
+        }.get(resp_seeds[0], 'None')
+        if self._verbose:
+            print("IK Solution SUCCESS - Valid Joint Solution Found from Seed Type: {0}".format(
+                seed_str))
+        # Format solution into Limb API-compatible dictionary
+        limb_joints = dict(zip(resp.joints[0].name, resp.joints[0].position))
+        if self._verbose:
+            print("IK Joint Solution:\n{0}".format(limb_joints))
+            print("------------------")
+    else:
+        rospy.logerr("INVALID POSE - No Valid Joint Solution Found.")
+        return False
+    return limb_joints
+
+
+JOINT_NAMES = ["e0", "e1", "s0", "s1", "w0", "w1", "w2"]
+
+
 class Merry(object):
 
-    curr_goal = None
-    curr_pose = None
-    latest_obstacles = []
-    tf = None
-    planner = None
-    tf_broadcaster = None
     closest_points = []
+    joint_states = []
 
     # initialize and enable the robot and the named limb
     def __init__(self, limb="right", speed=0.3, accuracy=baxter_interface.JOINT_ANGLE_TOLERANCE):
 
         rospy.init_node("merry")
-        # store reference to planner
-        #self.planner = planner
 
         rospy.sleep(1)
 
         # Create baxter_interface limb instance
         self._arm = limb
         self._limb = baxter_interface.Limb(self._arm)
+        self._joint_names = []
 
         # Parameters which will describe joint position moves
         self._speed = speed
         self._accuracy = accuracy
 
-        # Recorded waypoints
-        self._waypoints = list()
-
-        # Recording state
-        self._is_recording = False
+        ns = "ExternalTools/" + limb + "/PositionKinematicsNode/IKService"
+        self._iksvc = rospy.ServiceProxy(ns, SolvePositionIK)
+        rospy.wait_for_service(ns, 5.0)
 
         # Verify robot is enabled
         print("Getting robot state... ")
@@ -128,98 +163,25 @@ class Merry(object):
         # Create Navigator I/O
         self._navigator_io = baxter_interface.Navigator(self._arm)
 
-        #self.gripper = baxter_interface.Gripper(limb_name)
+        # self.gripper = baxter_interface.Gripper(limb_name)
+        self.joint_states_subscriber = rospy.Subscriber("robot/joint_states", JointState, self.joint_state_cb)
 
-        #self.curr_goal = G_HOME
         self.kinect_subscriber = rospy.Subscriber("kinect/depth/points", PointCloud2, self.kinect_cb)
         self.tf = tf.TransformListener()
 
-        self.bridge = CvBridge()
-        self.tf_broadcaster = tf.TransformBroadcaster()
+        # self.bridge = CvBridge()
 
-    def error_recovery(self, failed_move):
-        """
-        Tries to recover from APF planning local minima using the failed move as an example of what not to do.
-        Applies a bout of random perturbation to the current arm pose to change planning ability.
-        :return: status of recovery operation; "OK" on success, "ERROR" on error
-        """
-        status = ERROR
-        return status
-
-    def plan_and_execute(self):
-        """
-        Moves to the 3-D goal coordinates that
-        are given as a 3-tuple (x, y, z)
-        :param goal: a dictionary of coords and other goal data
-        :return: the status of the operation
-        """
-        status = OK
-        while status is OK:
-            # generate the next goal
-            next_move = self.generate_next_move()
-            # execute next step in movement
-            status = self.execute_next_move(next_move)
-            # check if the status is successful
-            if status is not OK:
-                # try to revisit move with perturbation
-                status = self.error_recovery(next_move)
-        return status
-
-    def find_closest_goal(self):
-        obs = self.latest_obstacles
-        closest_dist_to_bot = 1000000
-        closest_ob = None
-        for ob in obs:
-            pass
-        return closest_ob
-
-    def generate_next_move(self):
-        """
-        Generates the next move in a series of moves using an APF planning method.
-        :return: the next pose to move to in a series of APF planning steps
-        """
-        # store ref to obstacles
-        obs = self.latest_obstacles
-        closest_obstacle = self.find_closest_goal()
-        self.curr_goal = closest_obstacle
-        return self.curr_goal
-
-    def execute_next_move(self, next_move):
-        """
-        Moves the Baxter robot to the location of the next move in a series of APF planning steps.
-        :return: a status about the move execution; in success, "OK", and in error, "ERROR".
-        """
-        status = "OK"
-        rospy.sleep(1.0)
-
-        # Set joint position speed ratio for execution
-        self._limb.set_joint_position_speed(self._speed)
-
-        # execute next move
-        if not rospy.is_shutdown():
-            self._limb.move_to_joint_positions(next_move, timeout=20.0,
-                                               threshold=self._accuracy)
-            # Sleep for a few seconds between playback loops
-            rospy.sleep(3.0)
-        else:
-            status = ERROR
-
-        # Set joint position speed back to default
-        self._limb.set_joint_position_speed(0.3)
-        return status
-
-    def get_current_goal(self):
-        # retrieve current pose from endpoint
-        current_pose = self._limb.endpoint_pose()
-        ik_pose = Pose()
-        ik_pose.position.x = current_pose['position'].x
-        ik_pose.position.y = current_pose['position'].y
-        ik_pose.position.z = current_pose['position'].z
-        ik_pose.orientation.x = current_pose['orientation'].x
-        ik_pose.orientation.y = current_pose['orientation'].y
-        ik_pose.orientation.z = current_pose['orientation'].z
-        ik_pose.orientation.w = current_pose['orientation'].w
-        return ik_pose
+    def joint_state_cb(self, data):
+        names = data.name
+        positions = data.position
+        velocities = data.velocity
+        efforts = data.effort
+        self.joint_states = dict()
+        for i in range(len(names)):
+            self.joint_states[names[i]] = dict()
+            self.joint_states[names[i]]["position"] = positions[i]
+            self.joint_states[names[i]]["velocity"] = velocities[i]
+            self.joint_states[names[i]]["effort"] = efforts[i]
 
     def kinect_cb(self, data, source="kinect_link", dest="right_gripper", max_dist=2):
         """
@@ -232,6 +194,145 @@ class Merry(object):
         self.closest_points = [p for p in transformed_points if math.sqrt(p.x**2 + p.y**2 + p.z**2) < max_dist]
         print "found closest points"
         print "there are this many close points: " + str(len(self.closest_points))
+
+    def get_joint_angles(self, side="right"):
+        """Gets the joint angle dictionary of the specified arm side."""
+        joint_angles = dict()
+        names = self.joint_states.keys()
+        for name in names:
+            if side in name:
+                if name not in self._joint_names:
+                    self._joint_names.append(name)
+                joint_angles[name] = (self.joint_states[name]["position"], self.joint_states[name]["velocity"],
+                                      self.joint_states[name]["effort"])
+        return joint_angles
+
+    def get_current_endpoint_pose(self):
+        # retrieve current pose from endpoint
+        current_pose = self._limb.endpoint_pose()
+        ik_pose = Pose()
+        ik_pose.position.x = current_pose['position'].x
+        ik_pose.position.y = current_pose['position'].y
+        ik_pose.position.z = current_pose['position'].z
+        ik_pose.orientation.x = current_pose['orientation'].x
+        ik_pose.orientation.y = current_pose['orientation'].y
+        ik_pose.orientation.z = current_pose['orientation'].z
+        ik_pose.orientation.w = current_pose['orientation'].w
+        return ik_pose
+
+    def generate_goal_angles(self, dest_point):
+        """Uses inverse kinematics to generate joint angles at destination point."""
+        ik_pose = Pose()
+        ik_pose.position.x = dest_point[0]
+        ik_pose.position.y = dest_point[1]
+        ik_pose.position.z = dest_point[2]
+        current_pose = self._limb.endpoint_pose()
+        ik_pose.orientation.x = current_pose['orientation'].x
+        ik_pose.orientation.y = current_pose['orientation'].y
+        ik_pose.orientation.z = current_pose['orientation'].z
+        ik_pose.orientation.w = current_pose['orientation'].w
+        joint_angles = ik_request(ik_pose)
+        return joint_angles
+
+    def approach(self, points):
+        """
+        Attempts to successfully visit one of the provided points in fifo order. Will try another point if one fails.
+        :param points: possible 3d points in the frame of the chosen (left or right) gripper to move to
+        :return: status of attempt to approach at least one of the specified points based on the success of the planning
+        """
+        status = ERROR
+        # approach the closest point then backups if needed
+        for p in points:
+            status = self.plan_and_execute(p)
+            if status is OK:
+                break
+        return status
+
+    def get_critical_points_of_obstacles(self):
+        """
+        Considers the closest point the obstacle currently.
+        In the future, will use obstacle detection to find the critical points of obstacles nearest to the robot.
+        :return: a list of critical points paired with distances to end effector for each of the nearest obstacles
+        """
+        closest_point = None
+        closest_dist = 15
+        # run obstacle detection on the points closest to the robot
+        for p in self.closest_points:
+            dist = math.sqrt(p.x**2 + p.y**2 + p.z**2)
+            if closest_point is not None and dist < closest_dist:
+                closest_dist = dist
+        return [(closest_point, closest_dist)]
+
+    def plan_and_execute(self, point, side="right"):
+        """
+        Moves to the 3-D goal coordinates that
+        are given as a 3-tuple (x, y, z)
+        :return: the status of the operation
+        """
+        # get obstacles
+        obstacles = self.get_critical_points_of_obstacles()
+        # generate the next goal
+        next_joint_angles = self.generate_next_joint_angles(obstacles, side)
+        # execute next step in movement
+        status = self.move_to_next_angles(next_joint_angles)
+        # check if the status is successful
+        if status is not OK:
+            # try to revisit move with perturbation
+            status = self.error_recovery(next_joint_angles, point)
+        return status
+
+    def generate_next_joint_angles(self, obs, side="right"):
+        """
+        Generates the next move in a series of moves using an APF planning method
+        by means of the current robot joint states and the desired goal point.
+        :param obs: the obstacles for the planner to avoid
+        :param side: the arm side of the robot
+        :return: the next pose to move to in a series of APF planning steps
+        """
+        return planner.plan(obs, self.get_joint_angles(side), side)
+
+    def move_to_next_angles(self, next_move):
+        """
+        Moves the Baxter robot to the location of the next move in a series of APF planning steps.
+        :return: a status about the move execution; in success, "OK", and in error, "ERROR".
+        """
+        status = "OK"
+        rospy.sleep(1.0)
+
+        # Set joint position speed ratio for execution
+        self._limb.set_joint_position_speed(self._speed)
+
+        # execute next move
+        if not rospy.is_shutdown() and next_move is not None:
+            self._limb.move_to_joint_positions(next_move, timeout=20.0,
+                                               threshold=self._accuracy)
+            # Sleep for a few seconds between playback loops
+            rospy.sleep(3.0)
+        else:
+            rospy.logerr("No joint angles provided for move to next joint angles. Not moving.")
+            status = ERROR
+
+        # Set joint position speed back to default
+        self._limb.set_joint_position_speed(0.3)
+        return status
+
+    def move_to_start(self, start_angles=None):
+        print("Moving the {0} arm to start pose...".format(self._limb))
+        if not start_angles:
+            start_angles = dict(zip(self._joint_names, [0]*7))
+        self.move_to_next_angles(start_angles)
+        # self.gripper_open()
+        rospy.sleep(1.0)
+        print("Running. Ctrl-c to quit")
+
+    def error_recovery(self, failed_move, point):
+        """
+        Tries to recover from APF planning local minima using the failed move as an example of what not to do.
+        Applies a bout of random perturbation to the current arm pose to change planning ability.
+        :return: status of recovery operation; "OK" on success, "ERROR" on error
+        """
+        status = ERROR
+        return status
 
     def clean_shutdown(self):
         print("\nExiting example...")
@@ -256,6 +357,7 @@ class Merry(object):
         Moves Merry's arms into the home pose.
         """
         pass
+
 
 if __name__ == '__main__':
     goal = None
--- planner.py
+++ planner.py
@@ -1,109 +1,149 @@
 import math
+import numpy as np
 import random
-import rospy
 import scipy
-from scipy.stats import norm
-virtualforce = False
 
 
-class PotentialFieldPlanner():
-    def __init__(self, p, obstacles, dt, m, fMax, diam):
-        rospy.init_node("baxter_potential_field")
-        self.diam = diam
-        self.fMax = fMax
-        self.m = m
-        self.dt = dt
-        self.vx = 0
-        self.vy = 0
-        self.vz = 0
-        self.x = p.x
-        self.y = p.y
-        self.z = p.z
-        self.obstacles = obstacles
-        self.target = obstacles[0]
+def calculate_cartesian_jacobian():
+    height = 6
+    width = 7
+    J0 = np.array([[[0]*width]*height])
+    c1 = 0
+    c2 = 0
+    c3 = 0
+    c4 = 0
+    c5 = 0
+    c6 = 0
+    s1 = 0
+    s2 = 0
+    s3 = 0
+    s4 = 0
+    s5 = 0
+    s6 = 0
+    L2 = 0
+    L3 = 0
+    L4 = 0
 
-    def in_range(self, ob, range_=0.6):
-        dist = ob.distanceSq(self)
-        if dist < range_:
-            return True
-        else:
-            return False
+    J11 = c1 * ((L3 + L4 * c6) * s3 * s4 + L4 * (c4 * c5 * s3 + c3 * c5 ) * s6) \
+         + s1 * s2 * (L2 + c4 * (L3 + L4 * c6) − L4 * c5 * s4 * s6) \
+         + c2 * (c3 * (L3 + L4 * c6) * s4 + L4 * (c3 * c4 * c5 − s3 * s5 * s6))
 
-    def update_position(self):
-        x_dir = 0
-        y_dir = 0
-        z_dir = 0
-        s_min = 200
+    J12 = c1 * (−c2 * (L2 + c4 * (L3 + L4 * c6) − L4 * c5 * s4 * s6) \
+         + s2 * (c3 * (L3 + L4 * c6) * s4 + L4 * (c3 * c4 * c5 − s3 * s4) * s6))
 
-        for ob in self.obstacles:
-            squared_dist = ob.distanceSq(self)
-            if squared_dist < 1:
-                math.sin(1)
-            dx = ob.charge * (ob.p.x - self.x) / squared_dist
-            dy = ob.charge * (ob.p.y - self.y) / squared_dist
-            dz = ob.charge * (ob.p.z - self.z) / squared_dist
-            x_dir += dx
-            y_dir += dy
-            z_dir += dz
+    J13 = (L3 + L4 * c6) * (c3 * s1 + c1 * c2 * s3) * s4 \
+         + L4 * (c4 * c5 * (c3 * s1 + c1 * c2 * s3) + (c1 * c2 * c3 − s1 * s3) * s5) * s6
 
-        norm_ = math.sqrt(x_dir*x_dir+y_dir*y_dir)
-        x_dir /= norm_
-        y_dir /= norm_
+    J14 = (L3 + L4 c6) (c4 s1 s3 + c1 (−c2 c3 c4 + s2 s4)) \
+         + L4 c5(−s1 s3 s4 + c1 * (c4 * s2 + c2 * c3 * s4)) * s6
 
-        for ob in self.obstacles:
-            if not self.in_range(ob):
-                continue
-            squared_dist = ob.distanceSq(self)
-            dx = (ob.p.x - self.x)
-            dy = (ob.p.y - self.y)
-            dz = (ob.p.z - self.z)
-            # add normal noise to simulate the sonar effect
-            dx = add_noise(dx, 0, 1)
-            dy = add_noise(dy, 0, 1)
-            dz = add_noise(dz, 0, 1)
-            safety = squared_dist / (dx * x_dir + dy * y_dir + dz * z_dir)
-            if (safety > 0) and (safety < s_min):
-                s_min = safety
+    J15 = L4 * (c5 * (c3 * s1 + c1 * c2 * s3) \
+         + (−c4 * s1 * s3 + c1 * (c2 * c3 * c4 − s2 * s4)) * s5) * s6
 
-        if s_min < 5:
-            oc = self.target.charge
-            self.target.charge *= s_min/5
-            print str(oc) + " DOWN TO " + str(self.target.charge)
+    J16 = L4 * (s1 * (c6 * (c4 * c5 * s3 + c3 s5) − s3 * s4 * s6) \
+         + c1 * (c6 * (c5 * s2 * s4 + c2(−c3 * c4 * c5 + s3 * s5)) \
+         + (c4 * s2 + c2 * c3 * s4) * s6))
 
-        if s_min > 100:
-            oc = self.target.charge
-            self.target.charge *= s_min/100
-            print str(oc) + " UP TO " + str(self.target.charge)
+    J17 = 0
 
-        yt_norm = s_min/2
-        vtx = yt_norm * x_dir
-        vty = yt_norm * y_dir
-        vtz = yt_norm * z_dir
-        fx = self.m * (vtx - self.vx) / self.dt
-        fy = self.m * (vty - self.vy) / self.dt
-        fz = self.m * (vtz - self.vz) / self.dt
-        f_norm = math.sqrt(fx * fx + fy * fy)
+    J21 = s1 * ((L3 + L4 * c6) * s3 * s4 + L4 * (c4 * c5 * s3 + c3 * s5) * s6) \
+         − c1 * (s2 * (L2 + c4 * (L3 + L4 * c6) − L4 * c5 * s4 * s6) \
+         + c2 * (c3 * (L3 + L4 * c6) * s4 + L4 * (c3 * c4 * c5 − s3 * s5) * s6))
 
-        if f_norm > self.fMax:
-            fx *= self.fMax / f_norm
-            fy *= self.fMax / f_norm
-            fz *= self.fMax / f_norm
+    J22 = s 1 (−c 2 (L 2 + c 4 (L 3 + L 4 c 6 ) − L 4 c 5 s 4 s 6 ) + s 2 (c 3 (L 3 + L 4 c 6 ) s 4 + L 4 (c 3 c 4 c 5 − s 3 s 5 ) s 6 ))
 
-        self.vx += (fx * self.dt) / self.m
-        self.vy += (fy * self.dt) / self.m
-        self.vz += (fz * self.dt) / self.m
+    J23 = − (L 3 + L 4 c 6 ) (c 1 c 3 − c 2 s 1 s 3 ) s 4 + L 4 (c 2 s 1 (c 4 c 5 s 3 + c 3 s 5 ) + c 1 (−c 3 c 4 c 5 + s 3 s 5 )) s 6 ,
 
-        # virtual force component
-        if virtualforce and self.target.charge < 1000 and self.x > 25 and self.y > 25:
-            print "Virtual Force"
-            self.target.charge *= s_min / 100
-            self.vx += 5
-        self.x += self.vx * self.dt
-        self.y += self.vy * self.dt
-        self.z += self.vz * self.dt
+    J24 = − (L 3 + L 4 c 6 ) (c 4 (c 2 c 3 s 1 + c 1 s 3 ) − s 1 s 2 s 4 ) + L 4 c 5 (c 4 s 1 s 2 + (c 2 c 3 s 1 + c 1 s 3 ) s 4 ) s 6
+
+    J25 = L 4 (c 1 (−c 3 c 5 + c 4 s 3 s 5 ) + s 1 (−s 2 s 4 s 5 + c 2 (c 5 s 3 + c 3 c 4 s 5 ))) s 6
+
+    J26 = L 4 (c 6 (c 5 (−c 4 (c 2 c 3 s 1 + c 1 s 3 ) + s 1 s 2 s 4 ) + (−c 1 c 3 + c 2 s 1 s 3 ) s 5 ) + (c 4 s 1 s 2 + (c 2 c 3 s 1 + c 1 s 3 ) s 4 ) s 6 )
+
+    J27 = 0
+
+    J31 = 0
+
+    J32 = −s 2 (L 2 + c 4 (L3 + L 4 c 6 ) − L 4 c 5 s 4 s 6 )
+        + c 2 (L 4 s 3 s 5 s 6 − c 3 ((L 3 + L 4 c 6 ) s 4 + L 4 c 4 c 5 s 6 ))
+
+    J33 = s 2 ((L 3 + L 4 c 6 ) s 3 s 4 + L 4 (c 4 c 5 s 3 + c 3 s 5 ) s 6 )
+
+    J34 = − (L 3 + L 4 c 6 ) (c 3 c 4 s 2 + c 2 s 4 ) + L 4 c 5 (−c 2 c 4 + c 3 s 2 s 4 ) s 6
+
+    J35 = L 4 (c 5 s 2 s 3 + (c 3 c 4 s 2 + c 2 s 4 ) s 5 ) s 6
+
+    J36 = −L 4 (c 6 (c 5 (c 3 c 4 s 2 + c 2 s 4 ) − s 2 s 3 s 5 ) + (c 2 c 4 − c 3 s 2 s 4 ) s 6 )
+
+    J37 = 0
+
+    J41 = 0
+
+    J42 = s1
+
+    J43 = -c1 * s2
+
+    J44 = c3 * s1 + c1 * c2 * s3
+
+    J45 = s 1 s 3 s 4 − c 1 (c 4 s 2 + c 2 c 3 s 4 )
+
+    J46 = c 5 (c 3 s 1 + c 1 c 2 s 3 )
+        + (−c 4 s 1 s 3 + c 1 (c 2 c 3 c 4 − s 2 s 4 )) s 5
+
+    J47 = s 1 (c 6 s 3 s 4 + (c 4 c 5 s 3 + c 3 s 5 ) s 6 )
+        + c 1 (−c 6 (c 4 s 2 + c 2 c 3 s 4 )
+        + (c 5 s 2 s 4 + c 2 (−c 3 c 4 c 5 + s 3 s 5 )) s 6 )
+
+    J51 = 0
+
+    J52 = c1
+
+    J53 = -s1 * s2
+
+    J54 = −c 1 c 3 + c 2 s 1 s 3
+
+    J55 = −c 4 s 1 s 2 − (c 2 c 3 s 1 + c 1 s 3 ) s 4
+
+    J56 = c 1 (−c 3 c 5 + c 4 s 3 s 5 )
+        + s 1 (−s 2 s 4 s 5 + c 2 (c 5 s 3 + c 3 c 4 s 5 ))
+
+    J57 = −c 6 (c 4 s 1 s 2 + (c 2 c 3 s 1 + c 1 s 3 ) s 4 )
+        − (c 5 (c 4 (c 2 c 3 s 1 + c 1 s 3 ) − s 1 s 2 s 4 ) + (c 1 c 3 − c 2 s 1 s 3 ) s 5 ) s 6
+
+    J61 = 1
+
+    J62 = 0
+
+    J63 = c2
+
+    J64 = s2 * s3
+
+    J65 = c 2 c 4 − c 3 s 2 s 4
+
+    J66 = c 5 s 2 s 3 + (c 3 c 4 s 2 + c 2 s 4 ) s 5
+
+    J67 = c 2 (c 4 c 6 − c 5 s 4 s 6 )
+        + s 2 (s 3 s 5 s 6 − c 3 (c 6 s 4 + c 4 c 5 s 6 ))
+
+    return J0
 
 
-def add_noise(val, mean, std_dev):
-    r = norm.rvs(mean, std_dev)
-    noise = std_dev * r + mean
-    return val + noise
+def plan(obstacles, jointangles, side):
+    # unpack joint angles
+    #names = ["".join([side, x]) for x in ["e0", "e1", "s0", "s1", "w0", "w1", "w2"]]
+
+    jointangles = dict()
+    for name in jointangles.keys():
+        if side in name:
+            jointangles[name.split("")[1]] = jointangles[name]["position"]
+    if len(obstacles) == 0:
+        # do simple planning, tracking is more precise
+        pass
+    else:
+        for ob in obstacles:
+            d0 = np.array([ob.x, ob.y, ob.z])
+            d0norm = np.linalg.norm(d0)
+            n0 = d0 / d0norm
+            n0t = n0.transpose()
+            J0 = calculatecartesianjacobian()
+            Jd0 = np.dot(n0t, J0)
